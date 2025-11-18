6bb9e1b6be48d66106edd26ed55d72d3d7278ee0
Marjorye <marjorye.p.picanco@gmail.com>
2025-11-18 19:57:19 -0300

    Carrinhofxml,css,controller,itemcontroller

---
+++ src/main/java/Controller/CarrinhoController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+
+public class CarrinhoController {
+
+    @FXML
+    private VBox listaCarrinho;
+
+    @FXML
+    private Label labelTotal;
+
+    private double total = 0.0;
+
+    public void adicionarItem(String nome, double preco, String imagem) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/itemCarrinho.fxml"));
+            Parent item = loader.load();
+
+            ItemCarrinhoController controller = loader.getController();
+            controller.setData(nome, preco, imagem, this);
+
+            listaCarrinho.getChildren().add(item);
+
+            total += preco;
+            atualizarTotal();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void atualizarTotal() {
+        labelTotal.setText(String.format("Total: R$ %.2f", total));
+    }
+
+    public void removerItem(double preco) {
+        total -= preco;
+        atualizarTotal();
+    }
+}
